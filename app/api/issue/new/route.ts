136d7f14bdd6dc642a113d4f9905f57887c42bfb
avayyyyyyy <jabhi465@gmail.com>
2024-02-18 19:15:41 +0530

    Bug Fixed

--- app/api/issue/new/route.ts
+++ app/api/issue/new/route.ts
@@ -9,27 +9,21 @@ const schema = z.object({
 
 export async function POST(request: NextRequest) {
 
-  let dummy = []
-
  try {
    const body = await request.json();
   const isValid = schema.safeParse(body); 
-  dummy.push("1")
   if (isValid.success) {
-    dummy.push("isValidated")
     const newIssue = await prisma?.issue.create({
       data: {
         title: body.title,
         description: body.description,
       },
     });
-    dummy.push("after new issue")
     if (newIssue) {
-      dummy.push("new issue")
       return NextResponse.json({ success: true }, { status: 200 });
     }
   } 
  } catch (error) {
-  return NextResponse.json({messgae:dummy}, {status: 400})
+  return NextResponse.json({messgae:error}, {status: 400})
  }
 }
